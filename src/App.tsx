975cbdc88a2141bd66d7a60275ec24e241a366c2
verodel2026 <hostelai@proton.me>
2026-05-04 20:22:12 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1484,7 +1484,7 @@ export default function App() {
  </div>
 
       <a
-      href="https://wa.me/34646244535"
+    href="https://wa.me/34646244535"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 z-50 w-14 h-14 bg-[#25D366] rounded-full flex items-center justify-center shadow-lg hover:scale-110 transition-transform duration-300"
